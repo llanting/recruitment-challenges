6ef583c9a78aeaf6bc2ee98a7022dea1b19c04f5
Lanette Lanting <lanettelanting@gmail.com>
2020-09-15 10:07:36 +0200

    solved counting-bits

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,17 @@
 function Count (input) {
-  throw new Error('Not implemented')
+  if (input < 0) throw new RangeError('The input must be a positive value')
+  const answer = []
+  const positions = []
+  const reversed = input.toString(2).split('').reverse().join('')
+  let numberOne = 0
+  for (let i = 0; i < reversed.length; i++) {
+    if (+reversed[i] === 1) {
+      positions.push(i)
+      numberOne++
+    }
+  }
+  answer.push(numberOne)
+  return answer.concat(positions)
 }
 
 module.exports = { Count }
